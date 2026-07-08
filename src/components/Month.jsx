1c66a4eb2c303cf93b43f45794e46bc404eebfb8
Priscilla-03.dev <priscilla.03.2018@gmail.com>
2026-07-08 15:54:35 -0500

    feat: agregar interfaz de las vistas Week, Month y Year

--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Month() {
   return (
-    <section>
-      Month
+    <section className='p-4'>
+      <div className='border border-black rounded bg-gray-700 p-2 text-left mb-2 flex justify-between gap-2'>
+        <div className='bg-gray-500 p-1 rounded min-w-[20%] text-center h-fit'>
+          <span className='text-4xl'>JUL</span> {/* Cambiamos el 30 por JUL para que sea coherente con el componente month */}            <span
+              style={{ writingMode: 'vertical-rl', transform: 'rotate(180deg)' }}
+              className='text-xs font-medium'>2026</span> {/* Cambiamos jul por 2026 para que sea coherente con el componente month */}
+        </div>
+        <div className='flex flex-wrap gap-2 justify-end'>
+          <span className='border border-black px-2 rounded bg-gray-500 inline-block h-fit'>Task one</span>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/Week.jsx
+++ src/components/Week.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
 function Week() {
-  return (
-    <section>
-      Week
-    </section>
-  )
+    return (
+        <section className='p-4'>
+            <div className='border border-black rounded bg-gray-700 p-2 text-left mb-2 flex justify-between gap-2'>
+                <div className='bg-gray-500 p-1 rounded min-w-[20%] text-center h-fit'>
+                    <span>Thursday</span>
+                    <div className='flex justify-center items-center'>
+                        <span className='text-4xl'>30</span>
+                        <span 
+                        style={{ writingMode: 'vertical-rl', transform: 'rotate(180deg)'}}
+                        className='text-xs font-medium'>JUL</span>
+                    </div>
+                </div>
+                <div className='flex flex-wrap gap-2 justify-end'>
+                    <span className='border border-black px-2 rounded bg-gray-500 inline-block h-fit'>Task one</span>
+                    <span className='border border-black px-2 rounded bg-gray-500 inline-block h-fit'>Task two</span>
+                    <span className='border border-black px-2 rounded bg-gray-500 inline-block h-fit'>Task three</span>
+                    <span className='border border-black px-2 rounded bg-gray-500 inline-block h-fit'>Task four</span>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Week
